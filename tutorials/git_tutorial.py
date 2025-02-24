32e6b37d21decd6e6321030054d9e0a2133b256c
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2025-02-24 18:22:54 +0100

    initial commit

---
+++ tutorials/git_tutorial.py
@@ -0,0 +1,14 @@
+"""
+GIT - 
+
+A git egy verzió kezelő rendszer. 
+A kód váltásainak kezelését oldja meg, illetve amikor egy feladaton
+egyszerre többen dolgozunk, ahhoz tud nagyon sok segíséget adni.
+
+Untracked állapot -  Az olyan fájlok, amelyeket a Git még nem követ (nincsenek a verziókövetésben).
+Staged állapot    -  Olyan fájlok, amelyeket már hozzáadtál a staging area-hoz (a file-okban vannak a változások)
+                     ez már egyfajta mentési pont
+Committed         -  A változtatások elmentésre kerültek a helyi repository-ba egy commit formájában
+
+
+"""
